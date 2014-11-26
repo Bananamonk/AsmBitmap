5c4fe3fa6d025a1a4b2a89392be5679f05e42f3a
Bananamonk <herrkarrol@gmail.com>
2014-11-26 22:42:47 +0100

    working app-startup
    works:
    C# filters: blur, sepia, sharpening
    TODO:
    C# filter: salt and pepper
    All ASM Filters

---
+++ JA_Image_Project/TimeFormats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JA_Image_Project
+{
+static class TimeFormats
+    {
+        public const string Default = @"hh\:mm\:ss\.fff";
+        public const string Long = @"d\.hh\:mm\:ss\.fff";
+    }
+}
